fd56d814fa502b4f19740197010a7c0a6371ddb7
Krishna Ganeriwal <ganeriwalk@gmail.com>
2017-02-20 07:56:49 +0530

    Update ActualData.js

--- ActualData.js
+++ ActualData.js
@@ -28,7 +28,7 @@ class ActualData extends Component
 
     componentDidMount()
     {
-       setInterval(() => {this.props.fetchUrlData(this.props.data)},200);    
+      // setInterval(() => {this.props.fetchUrlData(this.props.data)},500);    
     }
 
     checkFocus(event)
@@ -38,7 +38,9 @@ class ActualData extends Component
 
     checkBlur(h,i,j,event)
     {
+        console.log("here",this.x,event.target.innerText);
         var dupdata = this.props.data;
+        var me = this;
         var head = Object.keys(dupdata[0]);
         let alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
         var target = event.target.innerText;
@@ -52,16 +54,18 @@ class ActualData extends Component
                     this.props.checkIntegerAction(i,h,target);
                     if(dupdata[i][head[j]]["dep"].length)
                     {
-                        var depformula = []; 
+                        var depformula = [];
+                        var deep = []; 
                         depformula = dupdata[i][head[j]]["dep"];
-                        var deep = depformula[0];
-                        console.log(depformula,typeof(dupdata[i][head[j]]["dep"]));
-                        depformula.map(function(depf){
+                        depformula.map(function(r){
+                            deep.push(r);
+                        })
+                        deep.map(function(depf){
                             var p = parseInt(depf[1],10) - 1;
                             var t = depf[0];
                             var f = dupdata[p][t]["fx"];
-                            console.log(p,t,f);
-                            this.props.applyF(t,f,p,dupdata, "blue");
+                            console.log("p,t,f",p,t,f);
+                            me.props.applyF(t,f,p,dupdata, "blue");
                         }); 
                         
                     }  
@@ -92,47 +96,106 @@ class ActualData extends Component
             } 
         }  
     }
- 
-    refCallback(item)
+    
+    handleDoubleClick(event)
     {
-        if (item)
-        {  
-            var a = ReactDOM.findDOMNode(item);
+        var a = event.target;
             var fxBar = this.refs.theInput;
-            var flag = 0;
             var rowno = Number(a.className[1]);
             var header = (a.className[0].toLowerCase());
             var head = Object.keys(this.props.data[0]);
+            var data = "";
             var stream$ = Observable.fromEvent(a,'dblclick');
-            var data;
             stream$.subscribe((e) => {    
                 if(this.props.data[rowno -1][header]["fx"])
                 { 
                     data = this.props.data[rowno -1][header]["fx"];
-                    flag =1;
                 }
                 else
+                {  
                     data = this.props.data[rowno -1][header]["value"];
+                }
                 this.props.inputEdit(data,a.className);
             });
             var stream1$ = Observable.fromEvent(fxBar,'keyup')
                                     .debounceTime(750)
                                     .distinctUntilChanged();
             stream1$.subscribe((e) => {
-                console.log(flag,a);
-                if(flag == 1)
+                if(this.props.data[rowno -1][header]["fx"])
                 {
                     a.innerText = e.target.innerText;
                     this.props.data[rowno -1][header]["fx"] = e.target.innerText;
+                    //this.checkedBlur.bind(this,header,(rowno - 1),head.indexOf(header),a.innerText);
                 }
                 else
                 {
+                    this.checkedBlur.bind(this,header,(rowno - 1),head.indexOf(header),a.innerText);
                     a.innerText = e.target.innerText;
-                    this.props.data[rowno -1][header]["value"] = e.target.innerText;
+                    //this.props.data[rowno -1][header]["value"] = e.target.innerText;
                 }
             });
         }
-  }
+
+    checkedBlur(h,i,j,target)
+    {
+        console.log("there");
+        var dupdata = this.props.data;
+        var me = this;
+        var head = Object.keys(dupdata[0]);
+        let alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+        //var target = event.target.innerText;
+       // Observable.of(target).if(() => target == parseInt(target,10), console.log("Number"),console.log("NO"));
+        if(dupdata[i][head[j]]["value"] != target)
+        {
+            if( this.x[this.x.length - 1] != target)
+            {
+                if(target == parseInt(target,10))
+                {
+                    this.props.checkIntegerAction(i,h,target);
+                    if(dupdata[i][head[j]]["dep"].length)
+                    {
+                        var depformula = [];
+                        var deep = []; 
+                        depformula = dupdata[i][head[j]]["dep"];
+                        depformula.map(function(r){
+                            deep.push(r);
+                        })
+                        deep.map(function(depf){
+                            var p = parseInt(depf[1],10) - 1;
+                            var t = depf[0];
+                            var f = dupdata[p][t]["fx"];
+                            console.log(p,t,f);
+                            me.props.applyF(t,f,p,dupdata, "blue");
+                        }); 
+                        
+                    }  
+                }
+                else
+                {
+                    if(target[0] == '=' && alpha.indexOf(target[2])>-1 && alpha.indexOf(target[5])>-1 && target[1] == '(' && target[target.length -1]  == ')')
+                    {
+                        var op1i = target[3] - 1;
+                        var op2i = target[6] - 1;
+                        var op1j = alpha.indexOf(target[2]);
+                        var op2j = alpha.indexOf(target[5]);
+                        var operator = target[4];
+                        if( !(dupdata[op1i]) || !(dupdata[op2i]))
+                        {
+                            console.log("Operands are out of bounds");
+                        }
+                        else
+                        {
+                            this.props.applyF(h.h,target,i,dupdata,"blue");
+                        }
+                    }
+                    else
+                    {
+                      this.props.stringColor(h.h,i,target);  
+                    }
+                }   
+            } 
+        }  
+    }    
 
     saveData(){
         var dats = this.props.data;
@@ -192,11 +255,12 @@ class ActualData extends Component
                     <td
                         ref={function(e){if(e) e.contentEditable=true;}}
                         key={s}
-                        ref={this.refCallback.bind(this)}
+                        //ref={this.refCallback.bind(this)}
                         style = {{color:dupdata[h]['color']}}
                         className={s}
                         onFocus = {this.checkFocus.bind(this)}
                         onBlur = {this.checkBlur.bind(this,{h},i,j)}
+                        onClick = {this.handleDoubleClick.bind(this)}
                     >{dupdata[h]['value']}</td>
                 );   
             }))
@@ -211,7 +275,7 @@ class ActualData extends Component
                 <button id="save" onClick={this.saveData.bind(this)}>SAVE</button>
                 <button id="addRow" onClick={this.addRow.bind(this)}>ADD ROW</button>
                 <button id="addCol" onClick={this.addColumn.bind(this)}>ADD COLUMN</button>
-                <table><tr><td>fxbar:</td><td ref="theInput">{this.props.vad}</td></tr></table>
+                <table><tr><td>fxbar:</td><td contentEditable={true} ref="theInput" >{this.props.vad}</td></tr></table>
                 <table>
                     <thead>{this.renderHead(this.props.data)}</thead>
                     <tbody>{this.props.data.map(this.renderData)}</tbody>
